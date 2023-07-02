103e9029aee886ee6fe276006e0fae883f7a83e8
SrutiMi <114284489+SrutiMi@users.noreply.github.com>
2023-07-02 20:48:04 +0530

    counter changes to red when negative and green when positive

--- index.js
+++ index.js
@@ -13,16 +13,33 @@ let count=0;
 document.querySelector('.js-decrease').addEventListener('click',()=>{
   count--;
   document.querySelector('.js-counter-number').innerHTML=count;
+  updateCounterColor();
 });
 
 //For Increase button
 document.querySelector('.js-increase').addEventListener('click',()=>{
   count++;
   document.querySelector('.js-counter-number').innerHTML=count;
+  updateCounterColor();
 })
 
 //For Reset button
 document.querySelector('.js-reset').addEventListener('click',()=>{
   count=0;
   document.querySelector('.js-counter-number').innerHTML=count;
+  updateCounterColor();
 })
+
+const counterNumber=document.querySelector('.js-counter-number');
+
+//if the number is less than zero then add the class red and styling is done on that class
+function updateCounterColor(){
+      if(count<0){
+      counterNumber.classList.add('red');//this adds the class red
+      counterNumber.classList.remove('green');
+    }else{
+      counterNumber.classList.remove('red');
+      counterNumber.classList.add('green');
+    }
+}
+
